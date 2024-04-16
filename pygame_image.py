508a0aa6db39d6e67402952e300d8944badcb734
nakayama <c0b2310841@edu.teu.ac.jp>
2024-04-16 17:37:34 +0900

    練習2

---
+++ pygame_image.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    cg_img = pg.image.load("fig/3.png")
+    cg_img = pg.transform.flip(cg_img,True,False)
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+        x = tmr % 1600
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img,[-x+1600,200])
+        screen.blit(cg_img,[300,200])
+        pg.display.update()
+        tmr += 1        
+        clock.tick(200)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
